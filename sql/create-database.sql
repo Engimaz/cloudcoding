13ec3ade8be73f47bbff74ddc9272c2437d91a28
mahairong <2291649168@qq.com>
2024-01-04 09:39:27 +0800

    上传数据库文件

---
+++ sql/create-database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE `cloud-coding-auth` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
+CREATE DATABASE `cloud-coding-comment` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
+CREATE DATABASE `cloud-coding-course` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
+CREATE DATABASE `cloud-coding-dictionary` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
+CREATE DATABASE `cloud-coding-forum` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
+CREATE DATABASE `cloud-coding-program` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
+CREATE DATABASE `cloud-coding-resource` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
+CREATE DATABASE `nacos` CHARACTER SET 'utf8mb4' COLLATE 'utf8mb4_general_ci';
