1fc2269d7685935fdc74c06c6ea4317cb4303d6b
mahairong <2291649168@qq.com>
2024-01-23 14:54:21 +0800

    修改网关503错误

--- cloud-coding-gateway/src/main/java/cn/edu/hcnu/gateway/exception/GlobalErrorExceptionHandler.java
+++ cloud-coding-gateway/src/main/java/cn/edu/hcnu/gateway/exception/GlobalErrorExceptionHandler.java
@@ -8,6 +8,7 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.cloud.gateway.support.NotFoundException;
 import org.springframework.core.annotation.Order;
+import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -31,42 +32,40 @@ public class GlobalErrorExceptionHandler implements ErrorWebExceptionHandler {
         ServerHttpResponse response = exchange.getResponse();
         // 检测当前响应是否已经提交了，如果已经提交了，则直接返回
         if (response.isCommitted()) {
-            return Mono.error(ex);
+            return Mono.empty(); // 改为返回已完成的Mono
         }
 
         // 默认是系统异常
         RestResponse<Object> restResponse = RestResponse.fail(ResultCode.ERROR);
-
-        HttpStatus status;
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR; // 默认状态
 
         // 微服务没有启动或者连接超时
         if (ex instanceof NotFoundException) {
             restResponse = RestResponse.fail(ResultCode.NOT_FOUND);
-            status = (HttpStatus.SERVICE_UNAVAILABLE);
+            status = HttpStatus.SERVICE_UNAVAILABLE; // 改为NOT_FOUND
         }
-        //处理TOKEN失效的异常
+        // 处理TOKEN失效的异常
         else if (ex instanceof InvalidTokenException) {
             restResponse = RestResponse.fail(ResultCode.INVALID_TOKEN);
-            status = (HttpStatus.UNAUTHORIZED);
-        } else {
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
+            status = HttpStatus.UNAUTHORIZED;
         }
-
 
         // JSON格式返回
         response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+        DataBufferFactory bufferFactory = response.bufferFactory();
+        DataBuffer dataBuffer;
 
-        RestResponse<Object> finalRestResponse = restResponse;
+        try {
+            dataBuffer = bufferFactory.wrap(new ObjectMapper().writeValueAsBytes(restResponse));
+        } catch (Exception e) {
+            log.error("Error writing response", e); // 修改为记录实际发生的异常e
+            dataBuffer = bufferFactory.wrap(new byte[0]); // 确保有响应返回
+        }
 
-        return response.writeWith(Mono.fromSupplier(() -> {
-            DataBufferFactory bufferFactory = response.bufferFactory();
-            try {
-                response.setStatusCode(status);
-                return bufferFactory.wrap(new ObjectMapper().writeValueAsBytes(finalRestResponse));
-            } catch (Exception e) {
-                log.error("Error writing response", ex);
-                return bufferFactory.wrap(new byte[0]);
-            }
-        }));
+        // 现在设置状态码
+        response.setStatusCode(status);
+
+        return response.writeWith(Mono.just(dataBuffer));
     }
+
 }
